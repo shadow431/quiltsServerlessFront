35562ba66bb6535a148a239c461f4b870e3b3fc9
Peter L Gunter Jr <petlgunjr@gmail.com>
2019-11-12 12:20:31 -0800

    Populate form

--- src/schedule/EditSchedule.js
+++ src/schedule/EditSchedule.js
@@ -1,23 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../components/AdminNav";
 import { API } from "aws-amplify";
+import { FormGroup, FormControl, ControlLabel, Button } from "react-bootstrap";
 
-export default function EditSchedule (item) {
-  console.log('EditSchedule: ', item);
+export default function EditSchedule (props) {
+  const [id, setId ] = useState("");
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [location, setLocation] = useState();
+
+  useEffect (() => {
+    setId(props.location.state.props._id);
+    setName(props.location.state.props.name);
+    setDate(props.location.state.props.date);
+    setTime(props.location.state.props.time || "");
+    setLocation(props.location.state.props.location);
+  }, []);
+
+  function validateForm() {
+    return name.length > 0 &&
+      location.length > 0 &&
+      date.length > 0;
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleLocationChange(event) {
+    setLocation(event.target.value);
+  }
+
+  function handleDateChange(event) {
+    setDate(event.target.value);
+  }
+
+  function handleTimeChange(event) {
+    setTime(event.target.value);
+  }
 
   async function handleChange () {
-    try {
-      await API.put("quilts", `/admin/schedule/${item.location.state.props._id}`)
-    }
-    catch (e) {
-      console.log(e)
+    if(validateForm()) {
+      try {
+        await API.put("quilts", `/admin/schedule/${id}`);
+        props.history.push("/schedule");
+      }
+      catch (e) {
+        console.log(e);
+      }
     }
   }
   return (
     <div>
       <AdminNav />
-      <form>
-
+      <form onSubmit={handleChange}>
+        <FormGroup controlId="newevent">
+          <ControlLabel>Date</ControlLabel>
+          <FormControl onChange={handleDateChange} type="text" value={date}/>
+        </FormGroup>
+        <FormGroup controlId="newevent">
+          <ControlLabel>Hours Open</ControlLabel>
+          <FormControl onChange={handleTimeChange} type="text" value={time}/>
+        </FormGroup>
+        <FormGroup controlId="eventName">
+          <ControlLabel>Name</ControlLabel>
+          <FormControl onChange={handleNameChange} type="text" value={name}/>
+        </FormGroup>
+        <FormGroup controlId="eventLocation">
+          <ControlLabel>Location</ControlLabel>
+          <FormControl onChange={handleLocationChange} type="text" value={location}/>
+        </FormGroup>
+        <Button type="submit">Update</Button>
       </form>
     </div>
   )
