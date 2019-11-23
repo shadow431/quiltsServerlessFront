dd0d73f4d3fa111579b6ebbf54c1209e6d2c6d98
Peter L Gunter Jr <petlgunjr@gmail.com>
2019-11-22 18:05:27 -0800

    Change prices to string add medium bowl wrap

--- src/components/ImgBreakDown.js
+++ src/components/ImgBreakDown.js
@@ -4,45 +4,51 @@ const imgBreakDown = {
       prodType: "Baby Quilts",
       prodDesc: `<p>
       A MUST FOR THAT PRECIOUS LITTLE ONE IN YOUR LIFE..&nbsp; These quilts are just perfect for the crib, or to send along to day care or pre school. Made by hand, each quilt has eight squares of machine embroidery. The back of the quilt is flannel for warmth. All fabric is 100% cotton with Warm &amp; Natural batting inside. The binding is hand sewn on the back side.&nbsp; Fabric has been prewashed. Quilts are about 37" wide X45"long.</p><p>Can't find the colors or embroidery designs&nbsp;you want?&nbsp; Call or e-mail me. Special orders gladly accepted.</p><p>Please allow 3 weeks for special orders.</p><p><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">&nbsp; FREE SHIPPING ON ALL BABY QUILTS</span></span></span></p><p>&nbsp;</p><p></p><p id="note">*Please note: Contrasting fabric may vary from image shown*</p>`,
-      prodPrice: 0,
+      prodPrice: "95.00",
       prodImgLocation: ""
     },
     BKP: {
       prodType: "Backpack",
       prodDesc: "Self explanatory",
-      prodPrice: 35,
+      prodPrice: "35.00",
       colors: ["charcoal", "black", "navy", "royal blue", "red"],
       prodImgLocation: ""
     },
     BPB: {
       prodType: "Baked Potato Bag",
       prodDesc: `<p align="left"><b><font color="#800000" face="Verdana" size="5">THE BEST THING TO HAPPEN TO BAKED POTATOES!!!</font></b></p><p align="left"><b><font color="#111111" face="Verdana">Tired of over cooked ends on your microwave baked potatoes? Long for baked potatoes to be fluffy like when you used to bake them in the oven? </font></b></p><p align="left"><font color="#111111" face="Verdana">When you use my baked potato bag to bake your potatoes in the microwave, your potato will come out fluffy and evenly cooked just as if you baked them in the oven and used all that electricity. These bags are not only wonderful for potatoes, you can also use them to cook yams, corn on the cob, squash, sweet potatoes, artichokes and more.</font></p><p align="left"><span style="color: #0000ff"><font face="Verdana"><span style="font-size: 20px"><span style="background-color: #00ff00">&nbsp; FREE SHIPPING ON ORDERS $60.00 AND OVER.</span></span></font></span></p>`,
-      prodPrice: 10,
+      prodPrice: "10.00",
       prodImgLocation: "BakedPotatoBag.jpg"
     },
     BWL: {
       prodType: "Large Bowl Wrap",
       prodDesc: `<p style="font-size: 20px">The perfect companion for that bowl of soup or hot cereal you heat in the microwave. Never burn your hands again trying to pick up that bowl. Like ice cream? Tired of freezing your hands?&nbsp;&nbsp;Sit your dish in THE BOWL WRAP! Entertaining?&nbsp;Set that bowl of candy or peanuts in THE BOWL WRAP!&nbsp;Each&nbsp;BOWL WRAP&nbsp;is&nbsp;handmade of 100% cotton, shaped like a bowl and&nbsp;pre-washed to prevent shrinkage.&nbsp;Pick from the many selections below.&nbsp;</p><p><span style="font-size: 20px"><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">&nbsp; FREE SHIPPING ON ORDERS $60.00 AND OVER.</span></span></span></span></p><p id="note">*Please note: Contrasting fabric may vary from image shown*</p>`,
-      prodPrice: 20,
+      prodPrice: "20.00",
+      prodImgLocation: "BowlWrap.jpg"
+    },
+    BWM: {
+      prodType: "Medium Bowl Wrap",
+      prodDesc: `<p style="font-size: 20px">The perfect companion for that bowl of soup or hot cereal you heat in the microwave. Never burn your hands again trying to pick up that bowl. Like ice cream? Tired of freezing your hands?&nbsp;&nbsp;Sit your dish in THE BOWL WRAP! Entertaining?&nbsp;Set that bowl of candy or peanuts in THE BOWL WRAP!&nbsp;Each&nbsp;BOWL WRAP&nbsp;is&nbsp;handmade of 100% cotton, shaped like a bowl and&nbsp;pre-washed to prevent shrinkage.&nbsp;Pick from the many selections below.&nbsp;</p><p><span style="font-size: 20px"><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">&nbsp; FREE SHIPPING ON ORDERS $60.00 AND OVER.</span></span></span></span></p><p id="note">*Please note: Contrasting fabric may vary from image shown*</p>`,
+      prodPrice: "15.00",
       prodImgLocation: "BowlWrap.jpg"
     },
     BWS: {
       prodType: "Small Bowl Wrap",
       prodDesc: `<p style="font-size: 20px">The perfect companion for that bowl of soup or hot cereal you heat in the microwave. Never burn your hands again trying to pick up that bowl. Like ice cream? Tired of freezing your hands?&nbsp;&nbsp;Sit your dish in THE BOWL WRAP! Entertaining?&nbsp;Set that bowl of candy or peanuts in THE BOWL WRAP!&nbsp;Each&nbsp;BOWL WRAP&nbsp;is&nbsp;handmade of 100% cotton, shaped like a bowl and&nbsp;pre-washed to prevent shrinkage.&nbsp;Pick from the many selections below.&nbsp;</p><p><span style="font-size: 20px"><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">&nbsp; FREE SHIPPING ON ORDERS $60.00 AND OVER.</span></span></span></span></p>`,
-      prodPrice: 10,
+      prodPrice: "10.00",
       prodImgLocation: "BowlWrap.jpg"
     },
     CAP: {
       prodType: "Caps",
       prodDesc: `<p><span style="font-size: 18px">Time to replace that worn out cap??&nbsp; Why not wear one with man's best friend on it.&nbsp; Two styles to choose from, like the unstructured type, order CP78, for those that like the firmer cap, order the structured CP80.&nbsp; 100% cotton twill. Both styles are adjustable.</span><span style="font-size: 18px">Four colors to choose from, hunter green, red, blue and khaki.&nbsp; These caps make a great Father's Day gift. If you don't see you favorite dog breed, just give us a call or send an e-mail.</span></p><p>&nbsp;</p><p><span style="font-size: 18px"><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">&nbsp;FREE SHIPPING ON ORDERS $60.00 AND OVER</span></span></span></span></p>`,
-      prodPrice: 20,
+      prodPrice: "20.00",
       colors: [],
       prodImgLocation: "Cap2.jpg"
     },
     COM: {
       prodType: "Computer Briefcase",
       prodDesc: "",
-      prodPrice: 50,
+      prodPrice: "50.00",
       colors: ["heather grey", "navy"],
       prodImgLocation: ""
     },
@@ -50,7 +56,7 @@ const imgBreakDown = {
       prodType: "Dog Quilts",
       prodDesc: `<p>Just in time for the Holidays!!&nbsp; The perfect gift for the dog or cat lover on your list. Makes a great gift for that pet in your life too.&nbsp; Each quilt is about 37"wide X 45"long,&nbsp;just right for most dogs or a lap quilt. There&nbsp;are eight squares of machine embroidery designs of the Dog Breed of your choice (if available). The reverse side of the quilt is flannel.&nbsp; All fabric is 100% cotton that has been pre-washed by us before the quilt is made. Each quilt is made in the USA by us.&nbsp; Custom quilts are welcome, so if you have more then one breed of dog, we can put more then one on a quilt. You can also choose colors.&nbsp; We try to have a good stock on hand, custom quilts will take up to 3 weeks to finish. Larger quilts available on request.&nbsp; Please call 360-599-1816 or e-mail, <a href="mailto:embroidery@quilts.ws">embroidery@quilts.ws</a> for special orders.&nbsp; GO TO THE "TOTE BAG" PAGE ON LEFT TO SEE THE DOG AND CAT DESIGNS</p><p>
       &nbsp;</p><p><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">FREE SHIPPING ON ALL DOG QUILTS</span></span></span></p>  <p></p><p id="note">*Please note: Contrasting fabric may vary from image shown*</p><u><b><i><font face="Verdana" size="4">Australian Shephard</font></i></b></u>`,
-      prodPrice: 95,
+      prodPrice: "95.00",
       prodImgLocation: ""
     },
     FAB: {
@@ -60,53 +66,53 @@ const imgBreakDown = {
     GRB: {
       prodType: "Grocery Bag AKA Tote Bag",
       prodDesc: `<p>Looking for a "tote bag" that won't let you down?? Look no further. Our bag is made of durable water resistant 600 demier polyester fabric, the inside can be wiped out with a damp cloth should something spill. Two small inside pockets and a water bottle pocket on the outside. Tired of your tote bag falling over and everything spilling out??&nbsp;&nbsp; Bag measures 14" wide X 12" high,&nbsp;6 1/2" deep.. With your favorite pet embroidered on front you can bring it along where ever you go. Comes in your choice of colors listed below. If you don't see your dog breed, please e-mail me. Most breeds available. Don't want a dog on this bag, special orders welcome. <span style="font-size: 16px">T-SHIRTS, SWEATSHIRTS, HOODIES ARE AVAILABLE. PLEASE CONTACT US BY PHONE OR E-MAIL FOR COLORS ETC.</span></p><p><span style="color: #0000ff"><span style="font-size: 16px">&nbsp;BAGS ARE ONLY $20.00 EACH. DESIGNS WILL VARY IN SIZE&nbsp;&nbsp; </span></span></p><p><span style="font-size: 16px"><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">&nbsp;&nbsp; FREE SHIPPING ON ORDERS $60.00 AND OVER</span></span></span></span></p>`,
-      prodPrice: 20,
+      prodPrice: "20.00",
       colors: ["red", "royal blue", "carolina blue", "navy", "lime green", "kelly green", "purple", "lavender", "light pink", "hot pink", "black", "charcoal grey", "turquoise", "tangerine", "stone"],
       prodImgLocation: ""
     },
     HOO: {
       prodType: "Hoodie",
       prodDesc: "Regular Hoodie",
-      prodPrice: 40,
+      prodPrice: "40.00",
       colors: ["red", "royal blue", "heather grey", "purple", "lavender", "forest green"],
       prodImgLocation: ""
     },
     HOZ: {
       prodType: "Zipper Hoodie",
       prodDesc: "Hoodie with a zipper",
-      prodPrice: 45,
+      prodPrice: "45.00",
       colors: ["red", "royal blue", "heather grey", "purple", "lavender", "forest green"],
       prodImgLocation: ""
     },
     OVM: {
       prodType: "Oven Mitt",
       prodDesc: `<p><u><b><i><font face="Verdana" size="5">ONE SIZE FITS ALL!!!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font></i></b></u></p><p><font face="Verdana">Finally an oven mitt that fits everyone, the perfect size. Well insulated to protect you from the heat.</font></p><p><font face="Verdana"><span style="font-size: 22px"><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">&nbsp; FREE SHIPPING ON ORDERS $60.00 AND OVER</span>.</span></span></span></font></p><p></p><p id="note">*Please note: Contrasting fabric may vary from image shown*</p>`,
-      prodPrice: 10,
+      prodPrice: "10.00",
       prodImgLocation: "OvenMitt.jpg"
     },
     PLT: {
       prodType: "Plate Wrap",
       prodDesc: "Use for microwaving plates",
-      prodPrice: 25,
+      prodPrice: "25.00",
       prodImgLocation: ""
     },
     SLN: {
       prodType: "Sling",
       prodDesc: "",
-      prodPrice: 15,
+      prodPrice: "15.00",
       colors: ["red", "royal blue", "lime green", "navy", "black"],
       prodImgLocation: ""
     },
     SWT: {
       prodType: "Sweatshirt",
       prodDesc: "",
-      prodPrice: 30,
+      prodPrice: "30.00",
       colors: ["red", "royal blue", "heather grey", "purple", "lavender", "forest green"]
     },
     TLB: {
       prodType: "Tortilla Bags",
       prodDesc: `<p><span style="font-size: 22px">The perfect bag for heating your tortilla's or rolls in the microwave.&nbsp; Bag is 10" across so the small and medium tortilla's fit perfectly,&nbsp;you can then&nbsp;put the bag on the table to keep them warm. Can also be used for that extra waffle or pancakes. Bags are all handmade from 100% cotton that has been prewashed to prevent shrinkage. Lots of designs to choose from.</span></p><p>&nbsp;</p><p><span style="font-size: 22px"><span style="font-size: 24px"><span style="color: #0000ff"><span style="background-color: #00ff00">&nbsp; FREE SHIPPING ON ORDERS $60.00 AND OVER</span></span></span></span></p>`,
-      prodPrice: 7.50,
+      prodPrice: "7.50",
       prodImgLocation: "TortillaBag.jpg"
     },
     TOW: {
@@ -117,28 +123,28 @@ const imgBreakDown = {
     TSL: {
       prodType: "Long Sleeve T-Shirt",
       prodDesc: "",
-      prodPrice: 25,
+      prodPrice: "25.00",
       colors: ["red", "royal blue", "heather grey", "purple", "lavender", "forest green"],
       prodImgLocation: ""
     },
     TSS: {
       prodType: "Short Sleeve T-Shirt",
       prodDesc: "",
-      prodPrice: 15,
+      prodPrice: "15.00",
       colors: ["red", "royal blue", "heather grey", "purple", "lavender", "forest green"],
       prodImgLocation: ""
     },
     VES: {
       prodType: "Vest",
       prodDesc: "",
-      prodPrice: 30,
+      prodPrice: "30.00",
       colors: ["red", "royal blue", "navy blue"],
       prodImgLocation: ""
     },
     ZZT: {
       prodType: "Zipper Tote Bag",
       prodDesc: "Your all in one tote bag with a zipper for secure transportation.",
-      prodPrice: 10,
+      prodPrice: "10.00",
       colors: ["red", "royal blue", "hot pink", "black"],
       prodImgLocation: ""
     }
