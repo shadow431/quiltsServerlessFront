43bf9c4c18ed35f0d17f75fbc141fd8d42fe3365
Peter L Gunter Jr <petlgunjr@gmail.com>
2019-12-03 10:54:29 -0800

    Fix multi render all graphics

--- src/components/RenderGraphics.js
+++ src/components/RenderGraphics.js
@@ -3,6 +3,20 @@ import { Col, Grid, Row, Thumbnail } from "react-bootstrap";
 import imgBreakDown from "./ImgBreakDown";
 
 export default function renderGraphic(graphicProps) {
+
+  async function loadGraphic() {
+    try {
+      const fabrics = await API.get("quilts", "/fabric");
+      setGraphics(fabrics);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        loadGraphic();
+        // return <ToastDemo />;
+      }
+    }
+  }
+
   return (
     <Grid fluid>
       {!graphicProps.graphicView === "all" ? (
@@ -15,15 +29,10 @@ export default function renderGraphic(graphicProps) {
       }
       {graphicProps.graphics.map((graphic, i) => {
         if (graphicProps.graphicView === "all") {
-          for (let i = 0; i < graphicProps.graphics.length;) {
-            let currSubCat = graphicProps.graphics.filter(function(subGraphic) {
-              return (subGraphic.subCat === graphic.subCat)
-            });
-            i = i + currSubCat.length;
             return (
               <Row>
                 <Col>
-                  <h2 style={{textDecoration:"underline"}}><strong>{imgBreakDown.subCat[currSubCat[0].type]}</strong></h2>
+                  <h2 style={{textDecoration:"underline"}}><strong>{imgBreakDown.subCat[graphicView]}</strong></h2>
                 </Col>
                 {currSubCat.map(currGraphic => {
                   return (
@@ -36,8 +45,7 @@ export default function renderGraphic(graphicProps) {
                 })}
               </Row>
             )
-          }
-        } else if(graphic.subCat === graphicProps.graphicView){
+          } else {
           return (
             <Col key={i} xs={12} sm={5} md={4} lg={4}>
               <Thumbnail className="renderThumb" key={i} src={graphic.imgUrl} onClick={() => graphicProps.handleGraphicChoice(graphic)} alt="Image to be added soon....">
