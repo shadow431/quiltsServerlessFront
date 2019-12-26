b610c0deff0fb446f275b7082825f7c3b87b062b
Peter L Gunter Jr <petlgunjr@gmail.com>
2019-12-26 13:57:59 -0800

    add break between categories in all view

--- src/components/RenderGraphics.js
+++ src/components/RenderGraphics.js
@@ -55,6 +55,7 @@ export default function RenderGraphics(props) {
                   }
                 })}
               </Row>
+              <hr />
             </React.Fragment>
           )
         })}
